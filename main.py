82d5e7fd0f513ad8172ba4340ce9648b44e0338e
EvgeniiMorozov <emorozov1101@gmail.com>
2021-08-22 10:27:21 +0500

    add app

---
+++ main.py
@@ -0,0 +1,42 @@
+from sqlite3 import connect
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/users", methods=["GET", "POST"])
+def add_user():
+    conn = connect("shop.sqlite")
+    cursor = conn.cursor()
+
+    if request.method == "GET":
+        data = {
+            "name": str(request.form.get("name")),
+            "email": str(request.form.get("email")),
+            "phone": str(request.form.get("phone")),
+        }
+
+        # print(
+        #     f"INSERT INTO customer (name, phone, email) "
+        #     f"VALUES (\"{data['name']}\", \"{data['phone']}\", \"{data['email']}\")"
+        # )
+
+        cursor.execute(
+            f"INSERT INTO customer (name, phone, email) VALUES (\"{data['name']}\", \"{data['phone']}\", \"{data['email']}\")"
+        )
+        conn.commit()
+
+        return render_template("users.html", **data)
+
+    return render_template("users.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=7001)
